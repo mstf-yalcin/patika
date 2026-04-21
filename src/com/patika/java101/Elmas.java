9a51891026dab34b25abc50dfa2e75a72838faaa
mstf-yalcin <mstf.yalcin@outlook.com>
2026-04-21 10:23:40 +0300

    java 101-Elmas

---
+++ src/com/patika/java101/Elmas.java
@@ -0,0 +1,31 @@
+package com.patika.java101;
+
+import java.util.Scanner;
+
+public class Elmas {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number :");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
